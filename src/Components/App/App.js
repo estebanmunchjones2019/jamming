10fa96d1a8dbc2aa3a1043d05bc3fce70279300f
Esteban Munch Jones <esteban.munch.jones@gmail.com>
2019-11-26 12:06:21 +0000

    hasta punto 73

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -46,6 +46,7 @@ export class App extends React.Component {
       this.removeTrack = this.removeTrack.bind(this);
       this.updatePlaylistName = this.updatePlaylistName.bind(this);
       this.savePlaylist = this.savePlaylist.bind(this);
+      this.search = this.search.bind(this);
   }
 
   addTrack(track){
@@ -74,7 +75,13 @@ export class App extends React.Component {
     }
 
     savePlaylist(){
+      const trackURIs = this.state.playlistTracks.map(track =>{
+        return track.uri;
+      })
+    }
 
+    search(term){
+      console.log(term);
     }
     
   render(){
@@ -82,7 +89,7 @@ export class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-        <SearchBar/>
+        <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults}/>
             <Playlist onSave={this.savePlaylist} onNameChange={this.updatePlaylistName} onRemove={this.removeTrack} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
